6f0c1c23add8dce152fc94634df82eb6f9664c25
Hesham <heshamusama@gmail.com>
2018-09-05 10:00:45 +0200

    test(catalog_service_tests): add tests case for getArticle

--- src/api/__tests__/catalog_service_tests.js
+++ src/api/__tests__/catalog_service_tests.js
@@ -4,12 +4,16 @@ import AtlasSDK from '../../index';
 
 const fs = require('fs');
 const path = require('path');
-const json = fs.readFileSync(path.join(__dirname, 'data/catalog_service_response.json'), 'utf8');
+const articleJson = fs.readFileSync(path.join(__dirname, 'data/catalog_service_get_article_response.json'), 'utf8');
+const articlesJson = fs.readFileSync(
+  path.join(__dirname, 'data/catalog_service_get_articles_response.json'),
+  'utf8',
+);
 const configJson = fs.readFileSync(path.join(__dirname, 'data/config_service_response.json'), 'utf8');
 
 test('Should fetch article successfully after configuring SDK', async t => {
   fetchMock.get('https://atlas-config-api.dc.zalan.do/api/config/CLIENT_ID-staging.json', configJson);
-  fetchMock.get('https://catalog_api.com/api/articles/AD112B0F6-A11?client_id=CLIENT_ID', json);
+  fetchMock.get('https://catalog_api.com/api/articles/AD112B0F6-A11?client_id=CLIENT_ID', articleJson);
 
   const sdk = await AtlasSDK.configure({
     client_id: 'CLIENT_ID',
@@ -24,6 +28,32 @@ test('Should fetch article successfully after configuring SDK', async t => {
   t.is(article.attributes[0].name, 'Internal material');
   t.is(article.attributes[0].values[0], 'textile');
   t.is(article.infos[0], 'Removable cover sole');
+});
+
+test('Should fetch articles successfully after configuring SDK', async t => {
+  fetchMock.get('https://atlas-config-api.dc.zalan.do/api/config/CLIENT_ID-staging.json', configJson);
+  fetchMock.get(
+    'https://catalog_api.com/api/articles?config_skus=AD112B0F6-A11,SO254C009-K12&client_id=CLIENT_ID',
+    articlesJson,
+  );
+
+  const sdk = await AtlasSDK.configure({
+    client_id: 'CLIENT_ID',
+    sales_channel: 'SALES_CHANNEL',
+    is_sandbox: true
+  });
+
+  const articles = await sdk.getArticles('AD112B0F6-A11,SO254C009-K12');
+
+  t.is(articles[0].id, 'AD112B0F6-A11');
+  t.is(articles[0].name, 'LOS ANGELES - Trainers - white');
+  t.is(articles[0].attributes[0].name, 'Internal material');
+  t.is(articles[0].attributes[0].values[0], 'textile');
+  t.is(articles[0].infos[0], 'Removable cover sole');
+  t.is(articles[1].id, 'SO254C009-K12');
+  t.is(articles[1].name, '8 PACK - Socken - stone mix');
+  t.is(articles[1].attributes[2].name, 'Muster');
+  t.is(articles[1].attributes[2].values[0], 'meliert');
 });
 
 test.afterEach.always(() => {
